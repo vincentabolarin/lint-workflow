a204b7e48516084afac08db1895e01abd5b799ec
Vincent Abolarin <vincentabolarin@gmail.com>
2023-05-22 22:31:13 +0100

    add comment [no ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,6 +4,8 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 
+//Comment
+
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
